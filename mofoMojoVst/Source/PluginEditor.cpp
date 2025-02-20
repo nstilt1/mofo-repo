b606c02c7156cbaa9c7826e84f8d542ebf541c7c
Noah Stiltner <nstilt1@lsu.edu>
2025-02-20 13:01:59 -0600

    added license info for modifyHostMenu

--- mofoMojoVst/Source/PluginEditor.cpp
+++ mofoMojoVst/Source/PluginEditor.cpp
@@ -3,6 +3,35 @@
 #include "Knob.h"
 #include <public.sdk/source/vst/vstcomponentbase.h>
 
+/**
+ * @brief Modifies the parameter context menu.
+ * 
+ * @author various authors from Surge
+ * This snippet was taken from https://github.com/surge-synthesizer/surge/blob/bcbffe5336dddb269fa24b713a8fb1eaa3d1bd5e/src/surge-xt/SurgeSynthEditor.cpp#L629
+ * which is licensed under the GPL License.
+ * 
+ * Their full license comment:
+ * 
+ * Surge XT - a free and open source hybrid synthesizer,
+ * built by Surge Synth Team
+ *
+ * Learn more at https://surge-synthesizer.github.io/
+ *
+ * Copyright 2018-2024, various authors, as described in the GitHub
+ * transaction log.
+ *
+ * Surge XT is released under the GNU General Public Licence v3
+ * or later (GPL-3.0-or-later). The license is found in the "LICENSE"
+ * file in the root of this repository, or at
+ * https://www.gnu.org/licenses/gpl-3.0.en.html
+ *
+ * Surge was a commercial product from 2004-2018, copyright and ownership
+ * held by Claes Johanson at Vember Audio during that period.
+ * Claes made Surge open source in September 2018.
+ *
+ * All source for Surge XT is available at
+ * https://github.com/surge-synthesizer/surge
+ */
 juce::PopupMenu MofoFilterAudioProcessorEditor::modifyHostMenu(juce::PopupMenu menu)
 {
     // make things look a bit nicer for our friends from Image-Line
